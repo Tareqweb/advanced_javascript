664cffd4506449e9b7e2b6217439f00854fde2e9
Tareqweb <18103045tareq@gmail.com>
2020-08-06 12:21:06 +0600

    null vs undefined

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -2,34 +2,40 @@
 // undefined
 
 
-// let pakhi;
-// console.log(pakhi);
+let pakhi;
+console.log(pakhi);
+// undefine
 
 
-// function add(num1,num2){
-//     console.log(num1+num2);
-//     // not given return
-//     // return
+function add(num1,num2){
+    console.log(num1+num2);
+    // not given return
+    // return
 
-// }
-// const result=add(10,20);
-// console.log(result);
-
-
-
-// function add(num1,num2){
-//     console.log(num1,num2);
-// }
-// const result=add(12);
-// console.log(result);
+}
+const result=add(10,20);
+console.log(result);
+// undefine
 
 
-// const premik={name:"smart boy sangkar",phone:"01430510"};
-// console.log(premik.gf);
+function add(num1,num2){
+    console.log(num1,num2);
+}
+const result=add(12);
+console.log(result);
+// undefine
 
 
-// const fun=undefined;
-// console.log(fun);
+const premik={name:"smart boy sangkar",phone:"01430510"};
+console.log(premik.gf);
+// undefine
+
+
+const fun=undefined;
+console.log(fun);
+// undefine
+
 
 let ages=[2,5,9];
 console.log(ages[11]);
+// undefine
